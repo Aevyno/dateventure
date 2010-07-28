0df31f6ae46a807d1c5b1db306d17df894ae177d
aevyno <aevyno@gmail.com>
2010-07-28 20:24:35 +0300

    new main page

---
+++ dateventure.py
@@ -0,0 +1,48 @@
+from google.appengine.ext import db
+from google.appengine.ext import search
+from google.appengine.api import users
+K_ytt_j_Sukupuolis=('Mies','Nainen','En kerro')
+alignment=('Mies','Nainen','En kerro')
+etsin=('Mies', 'Nainen', 'Ei')
+
+
+class ilmoitus(search.SearchableModel):
+    global alignment
+    Olen = db.StringProperty(choices=alignment)
+    Etsin = db.StringProperty()
+    Paikka = db.StringProperty()
+    Paikkakunta = db.StringProperty(default="Turku")
+    Datetime = db.DateTimeProperty()
+    Date = db.DateProperty()
+    Time = db.TimeProperty()
+    Kuvaus = db.StringProperty()
+    Vastattu = db.BooleanProperty(default=False)
+    vKuvaus = db.StringProperty()
+    Ilmoittaja = db.UserProperty()
+    Vastaaja = db.UserProperty()
+    Age = db.IntegerProperty(default=0)
+    Poistettu = db.BooleanProperty(default=False)
+
+      
+class palaute(search.SearchableModel):
+    # Palautteen lahettajan tiedot
+    sukupuoli = db.StringProperty(choices=K_ytt_j_Sukupuolis)
+    ika = db.StringProperty()
+
+    # Palautteen osat
+    kayttaisitko = db.BooleanProperty()
+    hyodyllisyys = db.TextProperty()
+    hyodyllisyysRating = db.IntegerProperty()
+    kaytettavyys = db.TextProperty()
+    kaytettavyysRating = db.IntegerProperty()
+    vapaaSana = db.TextProperty()
+    kehitys = db.TextProperty()
+
+
+    date = db.DateTimeProperty(auto_now_add=True)
+
+
+    
+
+#class selaus(search.SearchableModel):
+#    Lookup_1 = db.ReferenceProperty(ilmoitus,collection_name="selaus_1_set")
